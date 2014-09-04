76b1e8a8beec457492c6d4d781b411f2128f9695
Tobias Johansson <T_johansson92@hotmail.com>
2014-09-04 16:36:09 +0200

    updated hangman

--- Hangman/lib/Hangman.rb
+++ Hangman/lib/Hangman.rb
@@ -69,19 +69,22 @@ class Random_word
 		good_words = content.split.select { |word| word.length.between?(5,12) }
 		word = good_words[rand(good_words.size)].downcase.split("")
 		@random_word = word.map { |word| word.upcase}
+		@fail_guess = Array.new
 	end
 end
 
 class Play
 	def initialize
-		rand_word = Random_word.new
+		@rand_word = Random_word.new
 		@input_obj = User_input.new
 
-		@word = rand_word.random_word		
-		@input = input_obj.get_input 
+		@word = @rand_word.random_word		
+		#@input = @input_obj.get_input 
 
 		@hidden_array = Array.new(@word.length)
-		@word.each_index { |i| @hidden_array[i] = "-" }
+		@word.each_index { |i| @hidden_array[i] = "_" }
+		@fail_guess = Array.new
+		@used_letters = Array.new
 		check_if_correct(@hidden_array)
 	end
 
@@ -89,36 +92,46 @@ class Play
 
 		counter = 0
 
-		while counter != @word.length
+		while counter <= 10
 
-			current_guess = @input
+			current_guess = @input_obj.get_input
+
 
 			@word.each_with_index do |word, i|
-				if @input.include?(word)
-					hidden_array[i] = @input
+				if current_guess.include?(word)
+					hidden_array[i] = current_guess
+					@used_letters += current_guess
 				end
 			end
 
-			counter += 1
+
+
+			unless hidden_array.include?(current_guess)
+				print "\nThat letter isn't in the word\n"
+				puts current_guess
+				@fail_guess << current_guess
+				counter += 1
+			end
+
+			if used_letters(current_guess[-1]).include? (current_guess)
+				puts "You've already used that word!"
+			end
+
+
+			#print "#{@word}" + "\n"
+			#print "Used words: #{failed_guesses}\n\n"
+			print "\n#{hidden_array}\n"
+			print @fail_guess
+
 		end
-
-
-		print "#{@word}" + "\n"
-		print hidden_array
 
 	end
 
-	def time_until_hanged
-		counter = 0
-		if counter == 10
-			puts "He's dead now..you lose"
-		end
-		counter += 1
-	end
+
 
 end #end class Play
 
 
 p = Play.new
 
-#GÖRA IMORGON: försök gör så man kan knappa in fler bokstäver än bara en
+#GÖRA IMORGON: gör stor if sats, och skapa fler metoder för att underlätta 
