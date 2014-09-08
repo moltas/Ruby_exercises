084aa4c42cf113bec68408aeb7da0e320d24178f
Tobias Johansson <T_johansson92@hotmail.com>
2014-09-08 22:32:15 +0200

    Finished the Hangman game

--- Hangman/lib/Hangman.rb
+++ Hangman/lib/Hangman.rb
@@ -1,55 +1,9 @@
 
-
-
-
-
-
-#Skapar ett nytt tomt array med @word.length och fyller med temporära värden
-
-
-=begin
-def initialize
-	content = File.open("../dictionary.txt", "r") { |file| file.read }
-	good_words = content.split.select { |word| word.length.between?(5,12) }
-	@word = good_words[rand(good_words.size)].downcase.split("")
-	@hidden_array = Array.new(@word.length)
-	@word.each_index { |i| hidden_array[i] = "-" }
-
-
-	puts "\nMata in en bokstav: "
-	@input = gets.chomp
-end
-
-def check_correct?(input, hidden_array)
-
-	@word.each_with_index do |word, i|
-		if input.include?(word)
-			hidden_array[i] = input
-		end
-	end
-	print "#{@word}" + "\n"
-	print hidden_array
-
-end
-
-#måste lägga till sätt att ge respons till användaren
-
-def remaining_guesses(current_guess)
-	letters = ("a".."z").to_a
-
-
-
-end
-
-remaining_guesses(input)
-check_correct?(@input, hidden_array)
-
-=end
-
+#Klass som sparar användarinput
 class User_input
 
 	def get_input
-		puts "\nMata in en bokstav: "
+		puts "\nEnter a letter: "
 		input = gets.chomp
 		input.upcase
 	end
@@ -60,6 +14,7 @@ class User_input
 
 end
 
+#Klass som hämtar ett slumpat ord från en dictionary fil
 class Random_word
 
 	attr_accessor :random_word
@@ -69,69 +24,121 @@ class Random_word
 		good_words = content.split.select { |word| word.length.between?(5,12) }
 		word = good_words[rand(good_words.size)].downcase.split("")
 		@random_word = word.map { |word| word.upcase}
-		@fail_guess = Array.new
 	end
 end
 
-class Play
+
+class Game
 	def initialize
 		@rand_word = Random_word.new
-		@input_obj = User_input.new
-
+		@input = User_input.new
 		@word = @rand_word.random_word		
-		#@input = @input_obj.get_input 
-
 		@hidden_array = Array.new(@word.length)
 		@word.each_index { |i| @hidden_array[i] = "_" }
 		@fail_guess = Array.new
 		@used_letters = Array.new
-		check_if_correct(@hidden_array)
+		play
 	end
 
-	def check_if_correct(hidden_array)
+	#metod som kör spelet
+	def play
+	
+		times_left = 10 
 
-		counter = 0
+		while times_left > 0
 
-		while counter <= 10
+			#OUTPUT------------------------------		
+			puts "\n\n_______Right Guesses___________"
+			#print "Used words: #{failed_guesses}\n\n"
+			print "\n    #{@hidden_array.join(" ")}\n\n"
+			puts "_______Used Letters________________"
+			print "\n    #{@fail_guess.join(", ")}\n\n"
+			puts "___________________________________"
+			print "\nTries left: #{times_left}\n\n\n"
 
-			current_guess = @input_obj.get_input
 
+			current_guess = @input.get_input
 
-			@word.each_with_index do |word, i|
-				if current_guess.include?(word)
-					hidden_array[i] = current_guess
-					@used_letters += current_guess
+			if current_guess.length == 0
+				puts "You didn't type anything"
+				current_guess
+			elsif current_guess.length == 1
+
+				if check_already_exists?(current_guess) == true
+					puts "That letter is already used"
+					current_guess
+
+				elsif check_correct_answer?(current_guess) == true	
+
+					@word.each_with_index do |word, i|
+
+						if word.include? (current_guess)
+							@hidden_array[i] = current_guess
+							@used_letters << current_guess
+
+						end
+					end			
+				else
+					puts "\nWrong guess\n\n"
+					@used_letters << current_guess
+					@fail_guess << current_guess
+					times_left -= 1
 				end
 			end
 
 
-
-			unless hidden_array.include?(current_guess)
-				print "\nThat letter isn't in the word\n"
-				puts current_guess
-				@fail_guess << current_guess
-				counter += 1
+			if did_I_win? == true #kollar efter vinst
+				puts "\nYOU WON!!\n"				
+				puts "That's right the word is: #{@hidden_array.join()}"
+				break
 			end
 
-			if used_letters(current_guess[-1]).include? (current_guess)
-				puts "You've already used that word!"
-			end
-
-
-			#print "#{@word}" + "\n"
-			#print "Used words: #{failed_guesses}\n\n"
-			print "\n#{hidden_array}\n"
-			print @fail_guess
-
+		end #end while
+		
+		if did_I_lose?(times_left) == true #kollar efter förlust
+			puts "\nYou lost :((\n"
+			puts "The word was: #{@word.join()}"	
 		end
 
+	end #end method
+
+
+	#Kollar om input är rätt svar
+	def check_correct_answer?(input)
+		if @word.include? input
+			return true
+		end
+		return false
 	end
 
+	#Kollar om värdet redan har angets
+	def check_already_exists?(input)
+		if @used_letters.include? input
+			return true
+		end	
+		return false
+	end
+
+	#kollar om du har vunnit
+	def did_I_win?
+		if @hidden_array == @word
+			return true
+		end
+		return false
+	end
+
+	#Kollar om du har förlorat
+	def did_I_lose?(times_left)
+		if times_left == 0
+			return true
+		end
+		return false
+	end
 
 
 end #end class Play
 
 
-p = Play.new
+#instansierar nytt spel
+g = Game.new
 
-#GÖRA IMORGON: gör stor if sats, och skapa fler metoder för att underlätta 
